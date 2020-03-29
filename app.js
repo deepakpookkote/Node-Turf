3529902a7ca8bfdb8be8472c5ca8261fc086a2cd
Deepak Pookkote <deepak.pookkote@wallstreetenglish.com>
2020-03-29 13:18:59 +0530

    set default EJS as templating engine

--- app.js
+++ app.js
@@ -1,18 +1,9 @@
 const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
-// const expressHandleBars = require('express-handlebars');
 
 const app = express();
 
-// app.engine('hbs', expressHandleBars({
-//     extname: "hbs",
-//     defaultLayout: "main-layout",
-//     layoutsDir: "views/layouts/",
-// }));
-
-// app.set('view engine', 'pug');
-// app.set('view engine', 'hbs');
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
@@ -27,7 +18,6 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    //res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
     res.status(404).render('404', {pageTitle: 'Page Not Found!!'});
 })
 
